caf4574294e3c2cecf62d28a330a6a27875786ab
ross <92001561+z0r0z@users.noreply.github.com>
2024-12-13 16:34:35 +0800

    nits

--- test/TestSwapGuard.t.sol
+++ test/TestSwapGuard.t.sol
@@ -2,13 +2,12 @@
 pragma solidity ^0.8.19;
 
 import {Test, console} from "../lib/forge-std/src/Test.sol";
-interface ICTC {
-    function checkPrice(string calldata token) external view returns (uint256 price, string memory priceStr);
-}
+
 contract TestSwapGuard is Test {
     /// @dev This is an ETH receiver swapper that targets Univ3 USDC pool.
     address constant buyUSDC = 0x0000000000008765af4E1A776bdaBBFB2aBe67c8;
-    // @dev CTC Check the chain contract
+
+    /// @dev CTC - Check The Chain contract. Fetches onchain prices.
     address constant ctc = 0x0000000000cDC1F8d393415455E382c30FBc0a84;
 
     function setUp() public payable {
@@ -16,14 +15,23 @@ contract TestSwapGuard is Test {
     }
 
     function testSwapGuardCTC() public payable {
-        (uint256 price,) = ICTC(ctc).checkPrice("WETH");
-        
-        // Case 1: Sending 1 ETH with USDC price (-$5) minOut encoded:
-        (bool success,) = buyUSDC.call{value: 1 ether + price - 5e6}("");
-        assertTrue(success);
+        unchecked {
+            (uint256 price,) = ICTC(ctc).checkPrice("WETH");
 
-        // Case 2: Sending 1 ETH but requiring more than USDC price minOut (should revert):
-        (success,) = buyUSDC.call{value: 1 ether + price + 5e6}("");
-        assertFalse(success);
+            // Case 1: Sending 1 ETH with USDC price (-$5) minOut encoded:
+            (bool success,) = buyUSDC.call{value: 1 ether + price - 5e6}("");
+            assertTrue(success);
+
+            // Case 2: Sending 1 ETH but requiring more than USDC price minOut (should revert):
+            (success,) = buyUSDC.call{value: 1 ether + price + 5e6}("");
+            assertFalse(success);
+        }
     }
+}
+
+interface ICTC {
+    function checkPrice(string calldata token)
+        external
+        view
+        returns (uint256 price, string memory priceStr);
 }
